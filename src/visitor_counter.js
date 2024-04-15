d98410012b36fc491053b2a79b194e62cee3ad9a
Nicolas Marcy <nico@cooya.fr>
2024-04-15 14:09:50 +0200

    comment added

--- src/visitor_counter.js
+++ src/visitor_counter.js
@@ -98,6 +98,7 @@ function buildCounterId(todayDate, prefix, counterName) {
 	return todayDate ? `${prefix}-${counterName}-${todayDate}` : `${prefix}-${counterName}`
 }
 
+// http://detectmobilebrowsers.com/
 function isMobileDevice(userAgent) {
 	return (
 		/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(userAgent) ||
